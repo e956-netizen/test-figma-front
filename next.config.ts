b557b1ec3e2707a4d898cc3a0707bd066e170e61
Alex Alvarez <alex.alvarez@esmax.cl>
2026-03-24 20:16:29 -0300

    v2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  eslint: {
+    // Permite que el build termine aunque no se ejecute ESLint en el proceso de build.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
